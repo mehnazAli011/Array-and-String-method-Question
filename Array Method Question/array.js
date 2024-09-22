95930585426be38817d64e5b931b339522f2749e
unknown <mehnazm406@gmail.com>
2024-09-22 18:48:36 +0530

    Array methods Questions

--- Array Method Question/array.js
+++ Array Method Question/array.js
@@ -1,7 +1,7 @@
-Array method question for practice
+// Array method question for practice
 
-                                       easy Question
-Q1.Given the array integer, write a function find the maximum element in the array.
+//                                        easy Question
+// Q1.Given the array integer, write a function find the maximum element in the array.
 const arr = [1, 2, 3, 4, 5, 6];
 
 // =>1.Approch
@@ -40,11 +40,11 @@ function findTheMaxElmInArr(arr) {
   return max;
 }
 console.log(findTheMaxElmInArr(arr));
-===============================================================================
+// // ===============================================================================
 
-Q2.Given the array integer, write a function find the all even element in the array.
+// Q2.Given the array integer, write a function find the all even element in the array.
 
-=>1 Apprcoh!
+// =>1 Apprcoh!
 function findAllEvenElmInArr(arr) {
   return arr.filter((elm) => elm % 2 == 0);
 }
@@ -440,3 +440,69 @@ console.log(result);
 // const nums = [1, 1, 1, 2, 2, 3];
 // const k = 2;
 // console.log(topKFrequent(nums, k)); // Output: [1, 2]
+//=================================================================================
+// Q find the original array of prefix XOR
+// You are given an integer array pref of size n. Find and return the array arr of size n that satisfies:
+
+// pref[i] = arr[0] ^ arr[1] ^ ... ^ arr[i].
+// Note that ^ denotes the bitwise-xor operation.
+
+// It can be proven that the answer is unique.
+
+// Example 1:
+
+// Input: pref = [5,2,0,3,1]
+// Output: [5,7,2,3,2]
+// Explanation: From the array [5,7,2,3,2] we have the following:
+// - pref[0] = 5.
+// - pref[1] = 5 ^ 7 = 2.
+// - pref[2] = 5 ^ 7 ^ 2 = 0.
+// - pref[3] = 5 ^ 7 ^ 2 ^ 3 = 3.
+// - pref[4] = 5 ^ 7 ^ 2 ^ 3 ^ 2 = 1.
+// Example 2:
+
+// Input: pref = [13]
+// Output: [13]
+// Explanation: We have pref[0] = arr[0] = 13.
+
+var findArray = function (pref) {
+  return pref.map((val, idx) => {
+    if (idx === 0) {
+      return val;
+    } else {
+      return val ^ pref[idx - 1];
+    }
+  });
+};
+console.log(findArray([5, 2, 0, 3, 1]));
+console.log(findArray([13]));
+// =====================================================================================
+// Q decode XORed Array
+// There is a hidden integer array arr that consists of n non-negative integers.
+
+// It was encoded into another integer array encoded of length n - 1, such that encoded[i] = arr[i] XOR arr[i + 1]. For example, if arr = [1,0,2,1], then encoded = [1,2,3].
+
+// You are given the encoded array. You are also given an integer first, that is the first element of arr, i.e. arr[0].
+
+// Return the original array arr. It can be proved that the answer exists and is unique.
+
+// Example 1:
+
+// Input: encoded = [1,2,3], first = 1
+// Output: [1,0,2,1]
+// Explanation: If arr = [1,0,2,1], then first = 1 and encoded = [1 XOR 0, 0 XOR 2, 2 XOR 1] = [1,2,3]
+// Example 2:
+
+// Input: encoded = [6,2,7,3], first = 4
+// Output: [4,2,0,7,4]
+
+function deCode(encoded, first) {
+  let res = [first];
+  encoded.forEach((val, i) => res.push(res[i] ^ val));
+  return res;
+}
+console.log(deCode([1, 2, 3], 1)); //[1,0,2,1]
+console.log(deCode([6, 2, 7, 3], 4));
+
+
+// ===============================================================================
