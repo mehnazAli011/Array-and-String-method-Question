512228e9a819b2e70eae67a2a5fa5845893651c1
unknown <mehnazm406@gmail.com>
2024-10-10 19:02:09 +0530

    Array method Question

--- Array Method Question/array.js
+++ Array Method Question/array.js
@@ -505,78 +505,86 @@
 // console.log(deCode([6, 2, 7, 3], 4));
 
 // // ===============================================================================
- // Q Sort character by frequncy
-  //   Given a string s, sort it in decreasing order based on the frequency of the characters. The frequency of a character is the number of times it appears in the string.
-  
-  // Return the sorted string. If there are multiple answers, return any of them.
-  
-  // Example 1:
-  
-  // Input: s = "tree"
-  // Output: "eert"
-  // Explanation: 'e' appears twice while 'r' and 't' both appear once.
-  // So 'e' must appear before both 'r' and 't'. Therefore "eetr" is also a valid answer.
-  // Example 2:
-  
-  // Input: s = "cccaaa"
-  // Output: "aaaccc"
-  // Explanation: Both 'c' and 'a' appear three times, so both "cccaaa" and "aaaccc" are valid answers.
-  // Note that "cacaca" is incorrect, as the same characters must be together.
-  // Example 3:
-  
-  // Input: s = "Aabb"
-  // Output: "bbAa"
-  // Explanation: "bbaA" is also a valid answer, but "Aabb" is incorrect.
-  // Note that 'A' and 'a' are treated as two different characters.
-  
-  function frequencySort(s) {
-    let map = new Map();
-    for (let c of s) {
-      map.set(c, (map.get(c) || 0) + 1);
-    }
-    let arr = [...map.entries()]
-      .sort((a, b) => b[1] - a[1])
-      .map((c) => c[0].repeat(c[1]))
-      .join("");
-    return arr;
+// Q Sort character by frequncy
+//   Given a string s, sort it in decreasing order based on the frequency of the characters. The frequency of a character is the number of times it appears in the string.
+
+// Return the sorted string. If there are multiple answers, return any of them.
+
+// Example 1:
+
+// Input: s = "tree"
+// Output: "eert"
+// Explanation: 'e' appears twice while 'r' and 't' both appear once.
+// So 'e' must appear before both 'r' and 't'. Therefore "eetr" is also a valid answer.
+// Example 2:
+
+// Input: s = "cccaaa"
+// Output: "aaaccc"
+// Explanation: Both 'c' and 'a' appear three times, so both "cccaaa" and "aaaccc" are valid answers.
+// Note that "cacaca" is incorrect, as the same characters must be together.
+// Example 3:
+
+// Input: s = "Aabb"
+// Output: "bbAa"
+// Explanation: "bbaA" is also a valid answer, but "Aabb" is incorrect.
+// Note that 'A' and 'a' are treated as two different characters.
+
+function frequencySort(s) {
+  let map = new Map();
+  for (let c of s) {
+    map.set(c, (map.get(c) || 0) + 1);
   }
-  console.log(frequencySort("tree"));
-  console.log(frequencySort("cccaaa"));
-  console.log(frequencySort("Aabb"));
-  // // Time complexity:-O(n)
+  let arr = [...map.entries()]
+    .sort((a, b) => b[1] - a[1])
+    .map((c) => c[0].repeat(c[1]))
+    .join("");
+  return arr;
+}
+console.log(frequencySort("tree"));
+console.log(frequencySort("cccaaa"));
+console.log(frequencySort("Aabb"));
+// // Time complexity:-O(n)
 
-  // ===================================================================================
-  // Given the array nums, for each nums[i] find out how many numbers in the array are smaller than it. That is, for each nums[i] you have to count the number of valid j's such that j != i and nums[j] < nums[i].
+// ===================================================================================
+// Given the array nums, for each nums[i] find out how many numbers in the array are smaller than it. That is, for each nums[i] you have to count the number of valid j's such that j != i and nums[j] < nums[i].
 
-  // Return the answer in an array.
-  
-  // Example 1:
-  
-  // Input: nums = [8,1,2,2,3]
-  // Output: [4,0,1,1,3]
-  // Explanation:
-  // For nums[0]=8 there exist four smaller numbers than it (1, 2, 2 and 3).
-  // For nums[1]=1 does not exist any smaller number than it.
-  // For nums[2]=2 there exist one smaller number than it (1).
-  // For nums[3]=2 there exist one smaller number than it (1).
-  // For nums[4]=3 there exist three smaller numbers than it (1, 2 and 2).
-  // Example 2:
-  
-  // Input: nums = [6,5,4,8]
-  // Output: [2,1,0,3]
-  // Example 3:
-  
-  // Input: nums = [7,7,7,7]
-  // Output: [0,0,0,0]
-  
-  //Better Approach
-  function smallerNumbersThanCurrent(nums) {
-    let sorted = [...nums].sort((a, b) => a - b);
-   return nums.map(elm=>sorted.indexOf(elm))
-  }
-  console.log(smallerNumbersThanCurrent([8, 1, 2, 2, 3]));
-  console.log(smallerNumbersThanCurrent([6, 5, 4, 8]));
-  console.log(smallerNumbersThanCurrent([7, 7, 7, 7]))
-  //Time complexity:-O(n**2)
+// Return the answer in an array.
 
-  // ======================================================================================
+// Example 1:
+
+// Input: nums = [8,1,2,2,3]
+// Output: [4,0,1,1,3]
+// Explanation:
+// For nums[0]=8 there exist four smaller numbers than it (1, 2, 2 and 3).
+// For nums[1]=1 does not exist any smaller number than it.
+// For nums[2]=2 there exist one smaller number than it (1).
+// For nums[3]=2 there exist one smaller number than it (1).
+// For nums[4]=3 there exist three smaller numbers than it (1, 2 and 2).
+// Example 2:
+
+// Input: nums = [6,5,4,8]
+// Output: [2,1,0,3]
+// Example 3:
+
+// Input: nums = [7,7,7,7]
+// Output: [0,0,0,0]
+
+//Better Approach
+function smallerNumbersThanCurrent(nums) {
+  let sorted = [...nums].sort((a, b) => a - b);
+  return nums.map((elm) => sorted.indexOf(elm));
+}
+console.log(smallerNumbersThanCurrent([8, 1, 2, 2, 3]));
+console.log(smallerNumbersThanCurrent([6, 5, 4, 8]));
+console.log(smallerNumbersThanCurrent([7, 7, 7, 7]));
+//Time complexity:-O(n**2)
+
+// ======================================================================================
+//Q
+function addToArrayForm(arr, k) {
+  let string = (Number(arr.join("")) + k).toString();
+  return string.split("").map((elm) => Number(elm));
+}
+console.log(addToArrayForm([1, 2, 0, 0], 34));
+console.log(addToArrayForm([2, 7, 4], 181));
+console.log(addToArrayForm([2, 1, 5], 806));
