b3c69f47a4eee6f417f5b379de32f47e588a2072
unknown <mehnazm406@gmail.com>
2024-10-16 21:59:20 +0530

    Array method Question

--- Array Method Question/array.js
+++ Array Method Question/array.js
@@ -118,6 +118,7 @@ console.log(findSumOfArr(arr));
 // Q5 Find the givene array first even number.
 
 // =>1
+
 function firstEvenNumber(arr) {
   return arr.find((elm) => elm % 2 == 0);
 }
@@ -627,3 +628,49 @@ console.log(arraySing([-1, -2, -3, -4, 3, 2, 1]));
 console.log(arraySing([1, 5, 0, 2, -3]));
 console.log(arraySing([-1, 1, -1, 1, -1]));
 // =========================================================================================
+// / Q 9 Find if Digit Game Can Be Won
+// You are given an array of positive integers nums.
+
+// Alice and Bob are playing a game. In the game, Alice can choose either all single-digit numbers or all double-digit numbers from nums, and the rest of the numbers are given to Bob. Alice wins if the sum of her numbers is strictly greater than the sum of Bob's numbers.
+
+// Return true if Alice can win this game, otherwise, return false.
+
+// Example 1:
+
+// Input: nums = [1,2,3,4,10]
+
+// Output: false
+
+// Explanation:
+
+// Alice cannot win by choosing either single-digit or double-digit numbers.
+
+// Example 2:
+
+// Input: nums = [1,2,3,4,5,14]
+
+// Output: true
+
+// Explanation:
+
+// Alice can win by choosing single-digit numbers which have a sum equal to 15.
+
+// Example 3:
+
+// Input: nums = [5,5,5,25]
+
+// Output: true
+
+// Explanation:
+
+// Alice can win by choosing double-digit numbers which have a sum equal to 25.
+//Brute Approach
+function canAliceWin(nums) {
+  let single = nums.filter((elm) => elm < 10).reduce((acc, curr) => acc + curr);
+  let double = nums.reduce((acc, curr) => acc + curr) - single;
+  return single !== double;
+}
+console.log(canAliceWin([1, 2, 3, 4, 10]));
+console.log(canAliceWin([1, 2, 3, 4, 5, 14]));
+console.log(canAliceWin([5, 5, 5, 25]));
+// ======================================================================================
