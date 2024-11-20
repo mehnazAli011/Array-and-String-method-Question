ca20aec8c7940080c5427c8171e79f1683f3f6bf
unknown <mehnazm406@gmail.com>
2024-11-20 18:31:20 +0530

    String Question

--- String Question/qunestion.js
+++ String Question/qunestion.js
@@ -915,3 +915,30 @@ console.log(removeTrailingZeros("1720865079269529096765717822459"));
 
 //                <------------------------------------------------->
 
+//Q Shuffle String
+// You are given a string s and an integer array indices of the same length. The string s will be shuffled such that the character at the ith position moves to indices[i] in the shuffled string.
+
+// Return the shuffled string.
+
+// Example 1:
+
+// Input: s = "codeleet", indices = [4,5,6,7,0,2,1,3]
+// Output: "leetcode"
+// Explanation: As shown, "codeleet" becomes "leetcode" after shuffling.
+// Example 2:
+
+// Input: s = "abc", indices = [0,1,2]
+// Output: "abc"
+// Explanation: After shuffling, each character remains in its position.
+
+function shulling(s, indices) {
+  let res = [];
+  for (let i = 0; i < s.length; i++) {
+    res[indices[i]] = s[i];
+  }
+  return res.join("");
+}
+
+console.log(shulling("codeleet", [4, 5, 6, 7, 0, 2, 1, 3]));
+console.log(shulling("abc", [0, 1, 2]));
+//time complexity:O(n)
