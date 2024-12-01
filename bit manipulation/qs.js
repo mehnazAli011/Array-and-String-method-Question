98b13af82a4502784ed5849cf7f8c53d308c4185
unknown <mehnazm406@gmail.com>
2024-12-01 21:38:06 +0530

    DSA question

--- bit manipulation/qs.js
+++ bit manipulation/qs.js
@@ -327,3 +327,37 @@ console.log(xorOperation(5, 0));
 console.log(xorOperation(4, 3));
 // time complexity:O(n)
 
+//First letter to appear twice.
+
+// Given a string s consisting of lowercase English letters, return the first letter to appear twice.
+// A letter a appears twice before another letter b if the second occurrence of a is before the second occurrence of b.
+// s will contain at least one letter that appears twice.
+
+// Example 1:
+
+// Input: s = "abccbaacz"
+// Output: "c"
+// Explanation:
+// The letter 'a' appears on the indexes 0, 5 and 6.
+// The letter 'b' appears on the indexes 1 and 4.
+// The letter 'c' appears on the indexes 2, 3 and 7.
+// The letter 'z' appears on the index 8.
+// The letter 'c' is the first letter to appear twice, because out of all the letters the index of its second occurrence is the smallest.
+// Example 2:
+
+// Input: s = "abcdd"
+// Output: "d"
+// Explanation:
+// The only letter that appears twice is 'd' so we return 'd'.
+
+function repeatedChar(s) {
+  let obj = {};
+  for (let elm of s) {
+    if (!obj[elm]) {
+      obj[elm] = 1;
+    } else return elm;
+  }
+}
+console.log(repeatedChar("abccbaacz"));
+console.log(repeatedChar("abcdd"));
+//time complexity:O(n)
