2a3d2f4ef7dbc62cdcf400520087fdbe766be55e
unknown <mehnazm406@gmail.com>
2024-12-02 21:03:14 +0530

    DSA Question

--- bit manipulation/qs.js
+++ bit manipulation/qs.js
@@ -361,3 +361,81 @@ function repeatedChar(s) {
 console.log(repeatedChar("abccbaacz"));
 console.log(repeatedChar("abcdd"));
 //time complexity:O(n)
+
+//         <-------------------------------------->
+//Q Number of the even and odd bits
+// You are given a positive integer n.
+
+// Let even denote the number of even indices in the binary representation of n with value 1.
+
+// Let odd denote the number of odd indices in the binary representation of n with value 1.
+
+// Note that bits are indexed from right to left in the binary representation of a number.
+
+// Return the array [even, odd].
+
+// Example 1:
+
+// Input: n = 50
+
+// Output: [1,2]
+
+// Explanation:
+
+// The binary representation of 50 is 110010.
+
+// It contains 1 on indices 1, 4, and 5.
+
+// Example 2:
+
+// Input: n = 2
+
+// Output: [0,1]
+
+// Explanation:
+
+// The binary representation of 2 is 10.
+
+// It contains 1 only on index 1.
+
+//Brute force approach
+function evenOddBit(n) {
+  let even = 0;
+  let odd = 0;
+  let binary = "";
+  //convert into binary from
+  while (n > 0) {
+    if (n % 2 === 1) binary += "1";
+    else binary += "0";
+    n = Math.floor(n / 2);
+  }
+  //find value 1 at even index
+  for (let index in binary) {
+    if (index % 2 === 0 && binary[index] === "1") {
+      even++;
+    } else if (index % 2 !== 0 && binary[index] === "1") {
+      //find value 1 at odd index
+      odd++;
+    }
+  }
+  return [even, odd];
+}
+console.log(evenOddBit(50));
+console.log(evenOddBit(2));
+//time compleixty:O(log n)
+
+function evenOddBit(n) {
+  let even = 0;
+  let odd = 0;
+  let bit = n.toString(2).split("").reverse().map(Number); //convert into binary from
+  for (let i = 0; i < bit.length; i++) {
+    if (i % 2 == 0 && bit[i] === 1) {
+      even++;
+    } else if (i % 2 !== 0 && bit[i] == 1) {
+      odd++;
+    }
+  }
+  return [even, odd];
+}
+console.log(evenOddBit(50));
+console.log(evenOddBit(2));
